559dd163c5345779d54e277a7798f119c9cbbc08
kohlironey292-lab <kohlironey292@gmail.com>
2026-05-02 08:43:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 7250012103))
+OWNER_ID = int(getenv("OWNER_ID", 8541652449))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/lll_RONEY_THE_BRAND_lll")
